a769318a6b8d91fde0092a3e3a2134f994a882b8
Kevin Lagos <kevinlagos977@gmail.com>
2024-02-26 08:17:48 -0600

    crud

--- Parcial 1/ServidorExpressDB/index.js
+++ Parcial 1/ServidorExpressDB/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express()
 const mysql= require ('mysql2')
 const port = 3000
+app.use(express.json());
 
 const connection =  mysql.createConnection({
   host: 'localhost',
@@ -11,6 +12,7 @@ const connection =  mysql.createConnection({
 });
 
 app.get('/personas', (req, res, next) => {
+  
   if (typeof req.query.id === 'undefined') {
     connection.query(
       'SELECT * FROM datos',
@@ -19,10 +21,15 @@ app.get('/personas', (req, res, next) => {
           console.error(err);
           res.status(500).send('Error ');
           return;
+          connection.end()
+
         }
         console.log(results);
         console.log(fields);
         res.send(results);
+        connection.end()
+
+
       }
     );
   } else {
@@ -33,14 +40,93 @@ app.get('/personas', (req, res, next) => {
           console.error(err);
           res.status(500).send('Error no encontrado');
           return;
+          connection.end()
+
         }
         console.log(results);
         console.log(fields);
         res.send(results);
+        connection.end()
+
       }
     );
   }
 });
+app.delete('/personas', (req, res, next) => {
+  if (typeof req.query.id === 'undefined') {
+    res.status(400).send('Debe proporcionar un ID para eliminar un registro.');
+    return;
+  }
+
+  connection.query(
+    `DELETE FROM datos WHERE id = ${req.query.id}`,
+    function(err, results, fields) {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error al eliminar el registro');
+        return;
+        connection.end()
+
+      }
+      console.log(`Registro eliminado con ID ${req.query.id}`);
+      res.send('Registro eliminado correctamente');
+      connection.end()
+
+    }
+  );
+});
+
+app.put('/personas', (req, res, next) => {
+  const id = req.query.id;
+  const newData = req.body;
+
+  if (!newData || Object.keys(newData).length === 0) {
+    console.log(req.body);
+    res.status(400).send('Se requiere al menos un campo para modificar el registro.');
+    return;
+  }
+
+  connection.query(
+    'UPDATE datos SET ? WHERE id = ?',
+    [newData, id],
+    function(err, results, fields) {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error al modificar el registro');
+        return;
+      }
+      console.log(`Registro modificado con ID ${id}`);
+      res.send('Registro modificado correctamente');
+    }
+  );
+});
+app.post('/personas', (req, res, next) => {
+  const {  nombre, direccion, curp, nacionalidad, numero, ciudad, estado, codigo } = req.body;
+
+  // Verificar que se proporcionen todos los campos obligatorios
+  if ( !nombre || !direccion || !curp || !nacionalidad || !numero || !ciudad || !estado || !codigo) {
+    res.status(400).send('Se requieren todos los campos para crear un nuevo registro.');
+    return;
+  }
+
+  // Crear un nuevo registro en la base de datos
+  connection.query(
+    'INSERT INTO datos ( nombre, direccion, curp, nacionalidad, numero, ciudad, estado, codigo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+    [ nombre, direccion, curp, nacionalidad, numero, ciudad, estado, codigo],
+    function(err, results, fields) {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error al insertar el nuevo registro');
+        return;
+      }
+      console.log(`Nuevo registro insertado `);
+      res.send('Nuevo registro insertado correctamente');
+    }
+  );
+});
+
+
+
 
 
 // app.get('/personas', async (req, res,next) => {
