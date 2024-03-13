0e34cb3d4932f8aa747fb6be59c547f32e00a2fa
KEVINLAGOS <kevinlagos977@gmail.com>
2024-03-13 09:43:29 -0500

    Motor de busqueda

---
+++ Parcial 2/ServerTemplate/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express()
+const path = require("path")
+app.set('view engine', 'pug')
+app.set('views',path.join(__dirname,"views")) 
+app.get('/', (req, res) => {
+  res.render('index.pug', { nombre: 'Kevin', apellido: 'Lagos' })
+})
+ 
+app.listen(3001, () => {
+  console.log(`Example app listening on port 3001`)
+})
