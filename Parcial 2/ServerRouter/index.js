c7f0970ea971708332a080dffa4188c59c89d9fb
KEVINLAGOS <kevinlagos977@gmail.com>
2024-03-14 09:41:42 -0500

    modificando

--- Parcial 2/ServerRouter/index.js
+++ Parcial 2/ServerRouter/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const personasRouter = require('./routes/personas');
 const port = 3000;
-
-app.use(bodyParser.json());
-
 app.use('/personas', personasRouter);
-
+app.use(express.json())
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
