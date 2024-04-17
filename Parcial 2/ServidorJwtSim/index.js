8ebe2c033304a0b93e0d926eee5f5b0235e5c3b2
KEVINLAGOS <kevinlagos977@gmail.com>
2024-04-17 09:12:47 -0500

    mod

--- Parcial 2/ServidorJwtSim/index.js
+++ Parcial 2/ServidorJwtSim/index.js
@@ -13,14 +13,14 @@ app.get('/sistema',verificarToken,function(req,res,next){
     res.json({mensaje:"Acceso concedido a ruta sistema"})
 });
 
-app.listen(8081,function(){
-    console.log("Servidor express escuchando en el puerto 8081");
+app.listen(8082,function(){
+    console.log("Servidor express escuchando en el puerto 8082");
 });
 
 function verificarToken(req,res,next){
     console.log(req.headers.authorization);
     let token = req.headers.authorization;
-    jsonwebtoken.verify(token, 'claveSecreta',function(err,decoded){
+    jsonwebtoken.verify(token,'17112001',function(err,decoded){
         if(err){
             res.json({Error:"Acceso no concedido a ruta sistema"});
         }else{
